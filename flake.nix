9811528c40e08756bd0dc31f2b47a3e48b3fde4f
kokobd <contact@zelinf.net>
2023-03-07 11:54:28 +0800

    fix flake

--- flake.nix
+++ flake.nix
@@ -16,57 +16,59 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        lib = nixpkgs.lib;
-        supportedCompilers = [ "ghc925" "ghc944" "ghc8107" ];
-        makeOverlays = compilerNixName: [
-          haskellNix.overlay
-          (final: prev:
-            {
-              timeline = final.haskell-nix.project' {
-                src = ./.;
-                name = "timeline";
-                compiler-nix-name = compilerNixName;
-                shell = {
-                  additional = pkgs: [ pkgs.tasty-discover ];
-                  tools = {
-                    cabal = "latest";
-                    # haskell-language-server = {
-                    #   version = "latest";
-                    #   configureArgs = ''--constraint "haskell-language-server -dynamic"'';
-                    # };
-                    hlint = "latest";
-                    cabal-fmt = "latest";
-                    ormolu = "latest";
+    let
+      lib = nixpkgs.lib;
+      supportedCompilers = [ "ghc926" "ghc944" "ghc8107" ];
+      makeOverlays = compilerNixName: [
+        haskellNix.overlay
+        (final: prev:
+          {
+            timeline = final.haskell-nix.project' {
+              src = ./.;
+              name = "timeline";
+              compiler-nix-name = compilerNixName;
+              shell = {
+                additional = pkgs: [ pkgs.tasty-discover ];
+                tools = {
+                  cabal = "latest";
+                  haskell-language-server = {
+                    version = "latest";
+                    configureArgs = ''--constraint "haskell-language-server -dynamic"'';
                   };
-                  buildInputs = with prev; [
-                    rnix-lsp
-                    nixpkgs-fmt
-                  ];
-                  withHoogle = true;
+                  hlint = "latest";
+                  cabal-fmt = "latest";
+                  ormolu = "latest";
                 };
+                buildInputs = with prev; [
+                  rnix-lsp
+                  nixpkgs-fmt
+                ];
+                withHoogle = true;
               };
-            })
-        ];
-        makePkgs = compilerNixName: import nixpkgs {
+            };
+          })
+      ];
+    in
+    let
+      makeFlake = compilerNixName: flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = import nixpkgs {
           inherit system;
           inherit (haskellNix) config;
           overlays = makeOverlays compilerNixName;
         };
-        makeFlake = compilerNixName: (makePkgs compilerNixName).timeline.flake { };
-        defaultFlake = makeFlake (builtins.head supportedCompilers);
-      in
-      defaultFlake // builtins.listToAttrs
-        (
-          builtins.map
-            (compilerNixName: {
-              name = compilerNixName;
-              value = makeFlake compilerNixName;
-            })
-            supportedCompilers
-        )
-    );
+        in
+        pkgs.timeline.flake { });
+      defaultFlake = makeFlake (builtins.head supportedCompilers);
+    in
+    defaultFlake // builtins.listToAttrs
+      (
+        builtins.map
+          (compilerNixName: {
+            name = compilerNixName;
+            value = makeFlake compilerNixName;
+          })
+          supportedCompilers
+      );
 
   nixConfig = {
     allow-import-from-derivation = "true";
