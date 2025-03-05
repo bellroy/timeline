9c92519fb9cdae4994d3d1f6923eafff25718933
kokobd <contact@zelinf.net>
2025-03-05 18:46:05 -0500

    dependency update 20250305

--- flake.nix
+++ flake.nix
@@ -5,9 +5,18 @@
     bellroy-nix-foss.url = "github:bellroy/bellroy-nix-foss";
   };
 
-  outputs = inputs: inputs.bellroy-nix-foss.lib.haskellProject {
-    src = ./.;
-    supportedCompilers = [ "ghc92" "ghc94" "ghc96" "ghc98" "ghc910" ];
-    defaultCompiler = "ghc96";
-  };
+  outputs =
+    inputs:
+    inputs.bellroy-nix-foss.lib.haskellProject {
+      src = ./.;
+      supportedCompilers = [
+        "ghc92"
+        "ghc94"
+        "ghc96"
+        "ghc98"
+        "ghc910"
+        "ghc912"
+      ];
+      defaultCompiler = "ghc96";
+    };
 }
